6d741ee4db128b7c2758ad9102b5468f69ba2ea6
David <david.gutierrezcolodra@gmail.com>
2025-12-02 16:45:00 +0100

    Fix source URL for OCI Autonomous Databases module

--- database.tf
+++ database.tf
@@ -1,6 +1,6 @@
 module "oci_lz_autonomous_databases" {
   count  = var.autonomous_databases_configuration != null ? 1 : 0
-  source = "git::https://github.com/iblake/clickops-oci-adb-orchestrator.git//module"
+  source = "git::https://github.com/oci-clickops/clickops-oci-adb-orchestrator.gi//module"
   providers = {
     oci = oci
   }
