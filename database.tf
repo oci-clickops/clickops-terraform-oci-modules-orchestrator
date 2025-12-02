303fd8a6e2c3e25f90538d83f861d1f91a75589a
iblake <david.gutierrezcolodra@gmail.com>
2025-12-02 17:35:26 +0100

    Format terraform files

--- database.tf
+++ database.tf
@@ -1,11 +1,11 @@
 module "oci_lz_autonomous_databases" {
-    count  = var.autonomous_databases_configuration != null ? 1 : 0
-    source = "git::https://github.com/oci-clickops/clickops-oci-adb-orchestrator.git//module"
-    providers = {
-      oci = oci
-    }
-    autonomous_databases_configuration = var.autonomous_databases_configuration
-    compartments_dependency            = local.compartments_dependency
-    network_dependency                 = local.network_dependency
-    kms_dependency                     = local.kms_dependency
+  count  = var.autonomous_databases_configuration != null ? 1 : 0
+  source = "git::https://github.com/oci-clickops/clickops-oci-adb-orchestrator.git//module"
+  providers = {
+    oci = oci
   }
+  autonomous_databases_configuration = var.autonomous_databases_configuration
+  compartments_dependency            = local.compartments_dependency
+  network_dependency                 = local.network_dependency
+  kms_dependency                     = local.kms_dependency
+}
